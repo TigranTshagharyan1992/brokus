55cc7d34a8ed1402e0309242ac8ceb3785a549a2
Arik <arikgevorgyan23@gmail.com>
2025-03-17 21:34:55 +0400

    blogInner page markup added

--- resources/views/blogInner.blade.php
+++ resources/views/blogInner.blade.php
@@ -1,1 +1,218 @@
-blogInner
+@extends('layouts.app')
+
+@section('styles')
+    <!-- Page Css -->
+    <link rel="stylesheet" href="{{asset('assets/css/pages/blog-inner.css')}}">
+    <!-- ========================== -->
+@endsection
+
+@section('content')
+
+<div class="blog-inner">
+    <div class="row align-between">
+        <div class="column sm-12 lg-7">
+            <div class="blog-inner__main">
+                <div class="blog-inner__header">
+                    <div class="blog-inner__arrow">
+                        <i class="icon icon-arrow-left"></i>
+                    </div>
+                    <div class="blog-inner__heading">
+                        <div class="blog-inner__category font-bold text-14 text-sm-12">Բլոգ</div>
+                        <h1 class="blog-inner__title font-black text-32 text-md-26 text-sm-22">
+                            Ճշգրտվել է ապրանքների ցանկը, որոնց համար մաքսային հայտարարագրման ժամանակ
+                            կարող են խնդիրներ լինել
+                        </h1>
+                    </div>
+                </div>
+                <div class="blog-inner__img">
+                    <img width="929" height="528" src="{{asset('assets/img/content/blog.jpg')}}" alt="Blog">
+                </div>
+                <div class="blog-inner__info">
+                    <div class="info-pair">
+                        <div class="info-pair__item">
+                            <i class="icon icon-date"></i>
+                            <span>Փետրվար 16 2025</span>
+                        </div>
+                        <div class="info-pair__item">
+                            <i class="icon icon-clock"></i>
+                            <span>6 րոպե</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="blog-inner__body widgets">
+                    <div class="widget text-widget">
+                        Մաքսային հայտարարագրերի գործընթացը կարևորագույն տարրերից է միջազգային առևտրի և
+                        երկրի տնտեսական կայունության մեջ։ Այն հնարավորություն է տալիս պետություններին վերահսկել
+                        ներմուծվող և արտահանվող ապրանքների հոսքը՝ ապահովելով հարկային և անվտանգության պահանջներին
+                        համապատասխանություն։ Սակայն որոշ ապրանքներ կարող են բախվել խնդիրների՝ մաքսային հայտարարագրում,
+                        ինչը կարող է առաջացնել մի շարք խոչընդոտներ ինչպես ներմուծողների, այնպես էլ մաքսային մարմինների համար։
+                    </div>
+                    <div class="widget important-widget">
+                        Ապրանքների ճշգրտված ցանկը թույլ է տալիս մաքսային մարմիններին արդյունավետ վերահսկել ապրանքների
+                        դասակարգումը, ըստ որի որոշվում են մաքսային վճարները, պահանջվող թույլտվությունները և թույլատրելի
+                        մաքսային ձևակերպումները։
+                    </div>
+                    <div class="widget text-widget">
+                        Մաքսային հայտարարագրերի գործընթացը կարևորագույն տարրերից է միջազգային առևտրի և երկրի
+                        տնտեսական կայունության մեջ։ Այն հնարավորություն է տալիս պետություններին վերահսկել ներմուծվող և
+                        արտահանվող ապրանքների հոսքը՝ ապահովելով հարկային և անվտանգության պահանջներին համապատասխանություն:
+                        Սակայն որոշ ապրանքներ կարող են բախվել խնդիրների՝ մաքսային հայտարարագրում, ինչը կարող է առաջացնել
+                        մի շարք խոչընդոտներ ինչպես ներմուծողների, այնպես էլ մաքսային մարմինների համար։
+                        <br><br>
+                        <b>Ապրանքների ցանկի ճշգրտումը և դրա հետ կապված խնդիրները</b>
+                        <br><br>
+                        <b>Ապրանքի կոդավորման սխալներ</b>
+                        <br>
+                        Ապրանքները մաքսային համակարգում հաճախ դասակարգվում են հատուկ ծածկագրերի՝ HS (Harmonized System)
+                        կոդերով, որոնք նշում են ապրանքի տեսակն ու բնույթը։ Երբ ապրանքի դասակարգման ընթացքում կատարվում են
+                        սխալներ կամ անհստակություններ, մաքսային մարմինները կարող են մերժել հայտարարագրերը կամ պահանջել
+                        լրացուցիչ փաստաթղթեր։ Սա կարող է առաջացնել բարդություններ ինչպես մատակարարների, այնպես էլ մաքսային
+                        մարմինների համար, քանի որ յուրաքանչյուր սխալ կոդավորումը կարող է պահանջել նոր թեստեր կամ ընթացակարգեր։
+                        <br>
+                        <b>2.Արտահանման/ներմուծման սահմանափակումներ</b>
+                        <br>
+                        Որոշ ապրանքներ կարող են ունենալ հատուկ սահմանափակումներ՝ կապված արտահանման կամ ներմուծման
+                        թույլտվությունների հետ։ Օրինակ, ռազմավարական նշանակություն ունեցող ապրանքների՝ օրինակ, զենքերի կամ
+                        տեխնոլոգիաների ներմուծման/արտահանման վրա սահմանափակումները կարող են առաջացնել խնդիրներ, եթե
+                        մաքսային հայտարարագիրը չի համապատասխանում երկրի կանոնակարգերին կամ միջազգային համաձայնագրերին։
+                        Այդպիսի ապրանքների սխալ դասակարգումը կարող է հանգեցնել դրանց մերժման կամ թույլտվության երկարաձգման։
+                        <br>
+                        <b>3.Ապրանքի ծագման երկրի բացահայտում</b>
+                        <br>
+                        Մաքսային հայտարարագրերում չափազանց կարևոր է նշել ապրանքի ծագման երկիրը, քանի որ այն կարող է ազդել
+                        մաքսային վճարների և պարտավորությունների վրա։ Եթե ապրանքի ծագման երկիրը մաքսային հայտարարագրում նշված
+                        չէ կամ սխալ է, դա կարող է հանգեցնել այնպիսի խնդիրների, ինչպիսիք են՝ մաքսային արտոնությունների կորուստը
+                        կամ ապրանքի բեռի կասկածելիություն։
+                    </div>
+                    <div class="widget image-widget image-widget_left">
+                        <img width="304" height="192" src="{{asset('assets/img/content/widget.jpg')}}" alt="Widget">
+                        Մաքսային հայտարարագրերում չափազանց կարևոր է նշել ապրանքի ծագման երկիրը, քանի որ այն կարող է
+                        ազդել մաքսային վճարների և պարտավորությունների վրա։ Եթե ապրանքի ծագման երկիրը մաքսային
+                        հայտարարագրում նշված չէ կամ սխալ է, դա կարող է հանգեցնել այնպիսի խնդիրների, ինչպիսիք են՝
+                        մաքսային արտոնությունների կորուստը կամ ապրանքի բեռի կասկածելիություն։
+                        Ապրանքները մաքսային համակարգում հաճախ դասակարգվում են հատուկ ծածկագրերի՝ HS (Harmonized System) կոդերով,
+                        որոնք նշում են ապրանքի տեսակն ու բնույթը։ Երբ ապրանքի դասակարգման ընթացքում կատարվում են սխալներ։
+                    </div>
+                    <div class="widget text-widget">
+                        Մաքսային հայտարարագրերի գործընթացը կարևորագույն տարրերից է միջազգային առևտրի և
+                        երկրի տնտեսական կայունության մեջ։ Այն հնարավորություն է տալիս պետություններին վերահսկել
+                        ներմուծվող և արտահանվող ապրանքների հոսքը՝ ապահովելով հարկային և անվտանգության պահանջներին
+                        համապատասխանություն։ Սակայն որոշ ապրանքներ կարող են բախվել խնդիրների՝ մաքսային հայտարարագրում,
+                        ինչը կարող է առաջացնել մի շարք խոչընդոտներ ինչպես ներմուծողների, այնպես էլ մաքսային մարմինների համար։
+                    </div>
+                    <div class="widget image-widget image-widget_right">
+                        <img width="304" height="192" src="{{asset('assets/img/content/widget.jpg')}}" alt="Widget">
+                        Մաքսային հայտարարագրերում չափազանց կարևոր է նշել ապրանքի ծագման երկիրը, քանի որ այն կարող է
+                        ազդել մաքսային վճարների և պարտավորությունների վրա։ Եթե ապրանքի ծագման երկիրը մաքսային
+                        հայտարարագրում նշված չէ կամ սխալ է, դա կարող է հանգեցնել այնպիսի խնդիրների, ինչպիսիք են՝
+                        մաքսային արտոնությունների կորուստը կամ ապրանքի բեռի կասկածելիություն։
+                        Ապրանքները մաքսային համակարգում հաճախ դասակարգվում են հատուկ ծածկագրերի՝ HS (Harmonized System) կոդերով,
+                        որոնք նշում են ապրանքի տեսակն ու բնույթը։ Երբ ապրանքի դասակարգման ընթացքում կատարվում են սխալներ։
+                    </div>
+                    <div class="widget video-widget">
+                        <div class="video-widget__preview">
+                            <div class="video-widget__btn">
+                                <img width="64" height="64" src="{{asset('assets/img/play.svg')}}" alt="play">
+                            </div>
+                            <img width="929" height="528" src="{{asset('assets/img/content/blog.jpg')}}" alt="video">
+                        </div>
+                        <div class="video-widget__title">Ապրանքների ցանկի ճշգրտումը և դրա հետ կապված խնդիրները</div>
+                        <div class="video-widget__text">
+                            Մաքսային հայտարարագրերում չափազանց կարևոր է նշել ապրանքի ծագման երկիրը, քանի որ այն կարող է
+                            ազդել մաքսային վճարների և պարտավորությունների վրա։ Եթե ապրանքի ծագման երկիրը մաքսային
+                            հայտարարագրում նշված չէ կամ սխալ է, դա կարող է հանգեցնել այնպիսի խնդիրների, ինչպիսիք են՝
+                            մաքսային արտոնությունների կորուստը կամ ապրանքի բեռի կասկածելիություն։
+                            Ապրանքները մաքսային համակարգում հաճախ դասակարգվում են հատուկ ծածկագրերի՝ HS (Harmonized System) կոդերով,
+                            որոնք նշում են ապրանքի տեսակն ու բնույթը։ Երբ ապրանքի դասակարգման ընթացքում կատարվում են սխալներ։
+                        </div>
+                    </div>
+                    <div class="widget social-widget">
+                        <div class="social-widget__tittle">կիսվել այս հոդվածով</div>
+                        <div class="social-widget__icons">
+                            <div class="social-widget__icons-wrap">
+                                <a href="" class="btn btn_gray-border btn_badge btn_lg">
+                                    <i class="icon icon-facebook"></i>
+                                </a>
+                                <a href="" class="btn btn_gray-border btn_badge btn_lg">
+                                    <i class="icon icon-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column sm-12 lg-4">
+            <div class="suggested-blogs">
+                <div class="suggested-blogs__title">կարող է հետաքրքիր լինել</div>
+                <div class="suggested-blogs__list">
+                    <a href="{{ route('blogInner', ['lang' => app()->getLocale()]) }}" class="blog-card">
+                        <div class="blog-card__preview">
+                            <img width="516" height="296" src="{{asset('assets/img/content/blog-card.jpg')}}" alt="Blog">
+        
+                            <div class="blog-card__info info-pair">
+                                <div class="blog-card__info-item info-pair__item">
+                                    <i class="icon icon-date"></i>
+                                    <span>Փետրվար 16 2025</span>
+                                </div>
+                                <div class="blog-card__info-item info-pair__item">
+                                    <i class="icon icon-clock"></i>
+                                    <span>6 րոպե</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-card__body">
+                            <div class="blog-card__title">
+                                Ճշգրտվել է ապրանքների ցանկը, որոնց համար մաքսային հայտարարագրման ժամանակ
+                            </div>
+                        </div>
+                    </a>
+                    <a href="{{ route('blogInner', ['lang' => app()->getLocale()]) }}" class="blog-card">
+                        <div class="blog-card__preview">
+                            <img width="516" height="296" src="{{asset('assets/img/content/blog-card.jpg')}}" alt="Blog">
+        
+                            <div class="blog-card__info info-pair">
+                                <div class="blog-card__info-item info-pair__item">
+                                    <i class="icon icon-date"></i>
+                                    <span>Փետրվար 16 2025</span>
+                                </div>
+                                <div class="blog-card__info-item info-pair__item">
+                                    <i class="icon icon-clock"></i>
+                                    <span>6 րոպե</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-card__body">
+                            <div class="blog-card__title">
+                                Ճշգրտվել է ապրանքների ցանկը, որոնց համար մաքսային հայտարարագրման ժամանակ
+                            </div>
+                        </div>
+                    </a>
+                    <a href="{{ route('blogInner', ['lang' => app()->getLocale()]) }}" class="blog-card">
+                        <div class="blog-card__preview">
+                            <img width="516" height="296" src="{{asset('assets/img/content/blog-card.jpg')}}" alt="Blog">
+        
+                            <div class="blog-card__info info-pair">
+                                <div class="blog-card__info-item info-pair__item">
+                                    <i class="icon icon-date"></i>
+                                    <span>Փետրվար 16 2025</span>
+                                </div>
+                                <div class="blog-card__info-item info-pair__item">
+                                    <i class="icon icon-clock"></i>
+                                    <span>6 րոպե</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-card__body">
+                            <div class="blog-card__title">
+                                Ճշգրտվել է ապրանքների ցանկը, որոնց համար մաքսային հայտարարագրման ժամանակ
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
